d61ab048640f34409412895a7e5ce78bcb09468e
Liu-ko <33573204+Liu-ko@users.noreply.github.com>
2018-03-09 23:25:09 +0000

    Properly woking starting point for tutorial

---
+++ DoubleDi/Assets/Floating.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floating : MonoBehaviour
+{
+    public Vector3 angle;
+    public float speed;
+    private void Start()
+    {
+        angle = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), 0);
+        speed = Random.Range(2, 8);
+    }
+
+    void Update()
+    {
+        transform.Translate(angle * Time.deltaTime* speed, Space.World);
+    }
+
+    private void Bounce(Vector3 collisionNormal)
+    {
+        angle = Vector3.Reflect(angle.normalized, collisionNormal);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Bounce(collision.contacts[0].normal);
+    }
+}
---
+++ DoubleDi/Assets/OnClick.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+using System.Collections;
+
+public class OnClick : MonoBehaviour
+{
+    public GameObject coinPrefab;
+
+    public void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+            PutCoin(Input.mousePosition);
+    }
+
+    public void PutCoin(Vector2 mousePosition)
+    {
+        RaycastHit hit = RayFromCamera(mousePosition, 1000.0f);
+        GameObject.Instantiate(coinPrefab, hit.point, Quaternion.identity);
+    }
+
+    public RaycastHit RayFromCamera(Vector3 mousePosition, float rayLength)
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(mousePosition);
+        Physics.Raycast(ray, out hit, rayLength);
+        return hit;
+    }
+}
+
+
